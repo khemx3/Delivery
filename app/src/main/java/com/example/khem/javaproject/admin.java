bfd5bf7e4ad075300a292d8a9547c835d8344868
Phakapol Chuliang <phakapol_chuliang@yahoo.com>
2018-11-27 06:24:22 +0700

    admin part

---
+++ app/src/main/java/com/example/khem/javaproject/admin.java
@@ -0,0 +1,20 @@
+package com.example.khem.javaproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class admin extends AppCompatActivity {
+
+    private Button btn_add, btn_order;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+
+
+
+    }
+}
